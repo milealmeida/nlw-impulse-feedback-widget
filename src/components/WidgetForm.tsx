881ed2bb59e66bb839a78489add6ad0289415b3a
Milena Almeida <milenaalmeidadev@hotmail.com>
2022-05-04 18:12:18 -0400

    NLW Return - Mission Impulse: Stage 2 start

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,15 @@
+export function WidgetForm() {
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      <header>
+        <span>Deixe seu feedback</span>
+      </header>
+
+      <p>Hello World</p>
+
+      <footer>
+
+      </footer>
+    </div>
+  );
+}
